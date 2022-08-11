efdbbf3ada6128e3c4f7e96a7d097a9fe3ed6718
1999-julia <66686190+1999-julia@users.noreply.github.com>
2022-08-11 20:36:19 +0300

    Update GoogleTest.java

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class GoogleTest {
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GoogleTest {
+
+    @Test
+    public void userCanSearch() throws InterruptedException {
+        open("https://www.google.com");
+        new GooglePage().searchFor("Евтеева Юлия");
+
+        SearchResultsPage results = new SearchResultsPage();
+        results.getResults().shouldHave(sizeGreaterThan(1));
+        results.getResult(0).shouldHave(text("Евтеева Юлия"));
+        wait(500);
+    }
 }
