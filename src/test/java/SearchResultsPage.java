feb5256a6a9653c7950069e2586cb6d9130efdf6
1999-julia <66686190+1999-julia@users.noreply.github.com>
2022-08-11 20:34:53 +0300

    Update SearchResultsPage.java

--- src/test/java/SearchResultsPage.java
+++ src/test/java/SearchResultsPage.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SearchResultsPage {
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class SearchResultsPage {
+
+    public ElementsCollection getResults() {
+        return $$(".results .result");
+    }
+    public SelenideElement getResult(int index) {
+        return $("#r1-" + index);
+    }
 }
