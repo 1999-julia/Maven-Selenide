d403e6f5745eaf4173f3d7d60a5a2589cb7cd9bb
1999-julia <66686190+1999-julia@users.noreply.github.com>
2022-08-11 20:34:11 +0300

    Update GooglePage.java

--- src/test/java/GooglePage.java
+++ src/test/java/GooglePage.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class GooglePage {
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+public class GooglePage {
+
+    public void searchFor(String text) {
+        $(By.name("q")).val("Евтеева Юлия").pressEnter();
+
+    }
 }
